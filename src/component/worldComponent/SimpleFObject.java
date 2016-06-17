59421ab7b79ed6239599c3989be919f90ea2ac65
kcortes.95 <kcortes.95@gmail.com>
2016-06-17 15:06:35 -0300

    Mas javadoc y no termina mas

--- src/component/worldComponent/SimpleFObject.java
+++ src/component/worldComponent/SimpleFObject.java
@@ -3,11 +3,26 @@ package component.worldComponent;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * SimpleFObject.
+ * Es una clase que representa un objeto del mundo simple, o sea, que no es un objeto 
+ * compuesto por mas de uno.
+ * 
+ * Ejemplo: Pajaro, Life, Tube
+ */
 public abstract class SimpleFObject extends FlappyObject implements FlappyCrashable {
 
+	/** La posicion del objeto */
 	protected Vector2 position;
+	
 	protected Rectangle bounds;
 	
+	/**
+	 * Instantiates a new simple f object.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
 	public SimpleFObject(float x, float y) {
 		position = new Vector2(x, y);		
 	}
